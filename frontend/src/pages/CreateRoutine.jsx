6a99abd413bd099076991e8e751d6a5b73791d1d
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-11-14 11:41:55 +0530

    fix: routine timings

--- frontend/src/pages/CreateRoutine.jsx
+++ frontend/src/pages/CreateRoutine.jsx
@@ -17,9 +17,10 @@ const timeSlots = [
   "10:00 AM - 11:00 AM",
   "11:00 AM - 12:00 PM",
   "12:00 PM - 1:00 PM",
-  "2:00 PM - 3:00 PM",
-  "3:00 PM - 4:00 PM",
-  "4:00 PM - 5:00 PM",
+  "1:00 PM - 2:00 PM",
+  "2:30 PM - 3:30 PM",
+  "3:30 PM - 4:30 PM",
+  "4:30 PM - 5:30 PM",
 ];
 
 const CreateRoutine = () => {
